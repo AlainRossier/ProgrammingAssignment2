e34bd0563cdf1db4de238c6e0d09065513abb3bf
AlainRossier <alain6rossier@gmail.com>
2016-03-01 03:25:18 +0000

    AlainRossier

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The goal here is to compute the inverse of an invertible square matrix.
+## We also want to store it somewhere, so we don't have to compute the
+## inverse more than once, which can be useful. Indeed, for large matrices,
+## computing its inverse can take a lot of space and time.
 
-## Write a short comment describing this function
+## This function creates a new object, which consists of 4 functions that 
+## can set and access to the value of a given matrix which is stocked outside 
+## its environnement, the same for its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+	x<-NULL
+	set<-function(y) {
+		m<<-y
+		x<<-NULL
+	}
+	get<-function() {m}
+	setinv<-function(inv) {x<<-inv}
+	getinv<-function() {x}
+	list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function actually inverts a given matrix. The argument is this object
+## we've defined before, so we can check if the inverse of this matrix have been
+## already computed. In that case, the function the inverse and prints "getting
+## cached data". Otherwise, it simply computes the inverse of the matrix and stores
+## the inverse outside this environnement, so we can access to it later on.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+## Return a matrix that is the inverse of 'm'
+	x<-m$getinv()
+	if(!is.null(x)) {
+		message("getting cached data")
+		return(x)
+		}
+	data<-m$get()
+	x<-solve(data)
+	m$setinv(x)
+	x
 }
